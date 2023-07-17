f38e438358f7a7b4652a76a1ff28b0e831dc3607
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-07-17 12:20:12 -0300

    chore: export common link endpoints

---
+++ data/links.ts
@@ -0,0 +1,5 @@
+export const playLink = "/play";
+export const registerLink = "/register";
+export const accountLink = "/account";
+export const accountStatsLink = "/account/stats";
+export const leaderboardLink = "/leaderboards";
