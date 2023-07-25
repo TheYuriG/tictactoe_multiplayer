593b8803a997f00c65e908441f5287d7fce05651
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-07-25 10:08:23 -0300

    chore: changed "Play Now" button to anchor

--- components/UI/PlayButton.tsx
+++ components/UI/PlayButton.tsx
@@ -5,12 +5,12 @@ import { playLink } from "../../data/links.ts";
 
 export function PlayButton() {
   return (
-    <button
-      type="button"
+    <a
       href={playLink}
       class="px-3 py-2 bg-indigo-700 text-blue-200 transition-all duration-800 ease-in-out font-bold rounded-lg hover:(bg-indigo-800 scale-105) flex gap-2"
     >
-      Play now<IconPlayerPlay class="w-6 h-6" color="rgb(191, 219, 254)" />
-    </button>
+      Play now
+      <IconPlayerPlay class="w-6 h-6" color="rgb(191, 219, 254)" />
+    </a>
   );
 }
